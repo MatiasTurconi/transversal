ec14ed8e3a52ffa50a09ef20d296d7886bef563c
Vega Alberto Daniel <danielveg2013@gmail.com>
2023-05-13 12:14:02 -0300

    Actualizar mariaDB
    MariaDB 2.3.0 --->3.1.4

--- src/Control/Conexion.java
+++ src/Control/Conexion.java
@@ -12,7 +12,7 @@ import javax.swing.JOptionPane;
  * @author Usuario
  */
 public class Conexion {
-    private static String url="jdbc:mysql://localhost/universidad";
+    private static String url="jdbc:mariadb://localhost/universidad";
     private static String usuario="root";
     private static String password="";
 
